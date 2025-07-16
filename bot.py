f0d2207110947ef829ce238d06450a5411a16394
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-16 13:50:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,11 @@ from aiogram import Bot, Dispatcher
 import asyncio
 
 async def main():
+    print(f"TELEGRAM_TOKEN: {TELEGRAM_TOKEN} (type: {type(TELEGRAM_TOKEN)})") # отладка
+
+    if not isinstance(TELEGRAM_TOKEN, str) or not TELEGRAM_TOKEN:
+        raise ValueError("❌ TELEGRAM_TOKEN is not set or is not a string")
+        
     bot = Bot(token=TELEGRAM_TOKEN)
     dp = Dispatcher()
     setup_handlers(dp)
