3cd26beecbd8dcb16292c4431145d49abf7480a7
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-16 23:44:13 +0700

    Update user_manager.py

--- user_manager.py
+++ user_manager.py
@@ -11,7 +11,7 @@ db = client["neogpt"]
 users_collection = db["users"]
 
 def get_user(telegram_id: int) -> User:
-    data = users_collection.find_one({"telegram_id": telegram_id})
+    data = users_collection.find_one({"telegram_id": telegram_id})
     if data:
         user = User(**data)
         # Сброс лимитов, если день прошёл
