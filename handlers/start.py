e50bf9fcd31e40520d0a8f39f35569ee8f92bdc8
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-16 16:18:40 +0700

    Delete handlers/start.py

--- handlers/start.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-router = Router()
-
-@router.message(lambda msg: msg.text == "/start")
-async def start_handler(message: Message):
-    await message.answer("Привет! Я GPT-бот. Чем могу помочь?")
