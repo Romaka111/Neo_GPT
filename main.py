e87080804fd94c312471cc449ba8f8a1f2eef3e4
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-17 22:23:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import Update
+from aiogram.fsm.storage.memory import MemoryStorage
 from commands import register_command_handlers
 from message import register_message_handlers
-import yoomoney
 from config import TELEGRAM_TOKEN, BASE_URL
+import yoomoney
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
@@ -22,15 +22,12 @@ app.include_router(yoomoney.router)
 @app.on_event("startup")
 async def on_startup():
   await bot.set_webhook(f"{BASE_URL}/webhook")
-
-@app.on_event("shutdown")
-async def on_shutdown():
-  await bot.session.close()
+  print("Webhook установлен")
 
 @app.post("/webhook")
 async def telegram_webhook(request: Request):
   data = await request.json()
-  update = Update.model_validate(data)
+  update = Update(**data)
   await dp.feed_update(bot, update)
-  return {"status": "ok"}
+  return {"ok": True}
 
