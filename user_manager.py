2c894c63573f0f225a939f86227805812f4fe845
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-17 09:42:49 +0700

    Update user_manager.py

--- user_manager.py
+++ user_manager.py
@@ -44,22 +44,22 @@ def increment_usage(user: User, message: bool = False, image: bool = False):
         user.used_messages_today += 1
     if image:
         user.used_images_today += 1
-    save_user(user)
+    save_user(user)
 
 def can_use_feature(user: User, feature: str) -> bool:
-    config = get_subscription_config(user)
-    if feature == "message":
-        return user.used_messages_today < config["max_messages_per_day"]
-    if feature == "image":
-        return user.used_images_today < config["max_images_per_day"]
-    return False
+    config = get_subscription_config(user)
+    if feature == "message":
+        return user.used_messages_today < config["max_messages_per_day"]
+    if feature == "image":
+        return user.used_images_today < config["max_images_per_day"]
+    return False
 
 def add_to_memory(user: User, role: str, content: str):
-    user.memory.append(MemoryEntry(role=role, content=content))
-    if len(user.memory) > 20:
-        user.memory.pop(0)  # ограничиваем длину памяти
-    save_user(user)
+    user.memory.append(MemoryEntry(role=role, content=content))
+    if len(user.memory) > 20:
+        user.memory.pop(0)  # ограничиваем длину памяти
+    save_user(user)
 
 def clear_memory(user: User):
-    user.memory = []
-    save_user(user)
+    user.memory = []
+    save_user(user)
