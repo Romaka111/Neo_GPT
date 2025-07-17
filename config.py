a97cd65ab7db15be0e50cbf1815abee1bac67bb3
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-17 14:25:14 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+BASE_URL = os.getenv("BASE_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MONGO_URL = os.getenv("MONGO_URL")
+YOOMONEY_WALLET = os.getenv("YOOMONEY_WALLET")
+YOOMONEY_SECRET = os.getenv("YOOMONEY_SECRET")
