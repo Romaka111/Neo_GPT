b0aea3d98867fb163b0a8d63fad02a4980e2423b
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-17 10:50:31 +0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart, Command
 from user_manager import get_or_create_user
-from subscriptions import format_subscription_info
+from subscription import format_subscription_info
 
 router = Router()
 
