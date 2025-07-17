61e2bbd896763615cf96c2685d0fac98e15afe41
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-17 10:24:07 +0700

    Update user_manager.py

--- user_manager.py
+++ user_manager.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from pymongo import MongoClient
 from bson import ObjectId
 from user import User, MemoryEntry
-from app.core.constants import SUBSCRIPTION_CONFIG, SubscriptionType
+from constants import SUBSCRIPTION_CONFIG, SubscriptionType
 import os
 
 MONGO_URL = os.getenv("MONGO_URL")
